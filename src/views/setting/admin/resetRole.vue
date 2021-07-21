f6bfc06405a669690cdd2c5677981a5716c11cc4
zhaodan <zdailwh@sina.com>
2021-07-21 17:51:48 +0800

    “修改角色”操作 对于没有角色的用户 创建用户角色关联

--- src/views/setting/admin/resetRole.vue
+++ src/views/setting/admin/resetRole.vue
@@ -22,7 +22,7 @@
 </template>
 <script>
 import { getUserRole } from '@/api/admin'
-import { updateRoleUser } from '@/api/roleuser'
+import { createRoleUser, updateRoleUser } from '@/api/roleuser'
 export default {
   props: {
     dialogVisibleResetRole: {
@@ -71,16 +71,17 @@ export default {
           this.editform.roleId = response.role.id
         }
       }).catch((error) => {
-        this.$message({
-          message: error.message || '操作失败！',
-          type: 'error'
-        })
+        console.log(error.message)
       })
     },
     commit() {
       this.$refs.form.validate((valid) => {
         if (valid) {
-          this.updateRoleUser()
+          if (this.userRoleItem && this.userRoleItem.id) {
+            this.updateRoleUser()
+          } else {
+            this.createRoleUser()
+          }
         } else {
           console.log('error submit!!')
           return false
@@ -91,7 +92,21 @@ export default {
       this.loading = true
       updateRoleUser({ id: this.userRoleItem.id, roleId: this.editform.roleId }).then(response => {
         this.$message({
-          message: '编辑成功！',
+          message: '用户角色关联编辑成功！',
+          type: 'success'
+        })
+        this.loading = false
+        this.$emit('changeResetRoleVisible', false)
+        this.$emit('refresh')
+      }).catch(() => {
+        this.loading = false
+      })
+    },
+    createRoleUser() {
+      this.loading = true
+      createRoleUser({ userId: this.editItem.id, roleId: this.editform.roleId }).then(response => {
+        this.$message({
+          message: '用户角色关联创建成功！',
           type: 'success'
         })
         this.loading = false
